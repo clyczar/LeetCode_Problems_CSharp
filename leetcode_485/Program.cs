dd00724f51675777d9e98accacb1224a0a5ca177
clyczar <clyczar@gmail.com>
2023-10-20 12:30:14 +1030

    leetcode 485

---
+++ leetcode_485/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Solution
+{
+    public int FindMaxConsecutiveOnes(int[] nums)
+    {
+        if (nums.Length == 0)
+        {
+            return 0;
+        }
+
+        int result = 0;
+        int max = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == 1)
+            {
+                max += 1;
+            }
+            else
+            {
+                max = 0;
+            }
+            result = max > result ? max : result;
+
+        }
+        return result;
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] testcase = { 1, 1, 0, 1, 1, 1 };
+        Console.WriteLine(new Solution().FindMaxConsecutiveOnes(testcase));
+    }
+
+}
