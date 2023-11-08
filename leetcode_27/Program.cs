9722c5eaaaf47854b8b0991b69a23101d3f17c61
clyczar <clyczar@gmail.com>
2023-11-08 20:07:30 +1030

    leetcode 27

---
+++ leetcode_27/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode_27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 3, 2, 2, 3 };
+            int val = 3;
+            int result = new Program().RemoveElement(nums, val);
+            Console.WriteLine(result);
+        }
+
+        public int RemoveElement(int[] nums, int val)
+        {
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+
+            int left = 0;
+
+            for (int fast = 0; fast < nums.Length; fast++)
+            {
+                if (nums[fast] != val)
+                {
+                    nums[left] = nums[fast];
+                    left++;
+                }
+            }
+            return left;
+        }
+    }
+}
